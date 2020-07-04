6b7d1c1b5f4b3ffc7f9a82ec5b6d828000fbd924
George Boots <ggboots.99@gmail.com>
2020-07-04 05:24:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirsSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+
+
+}
+
+
+function increaseRankBy(n){
+
+}
+
+function deepestChild(){
+
+}
